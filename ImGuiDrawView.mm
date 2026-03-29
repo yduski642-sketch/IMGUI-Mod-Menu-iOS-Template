e3466956505c477c9c2093ea7c46f33e406e5947
yduski642-sketch <yduski642@gmail.com>
2026-03-29 11:23:14 -0800

    Update ImGuiDrawView.mm

--- ImGuiDrawView.mm
+++ ImGuiDrawView.mm
@@ -1,8 +1,3 @@
 #import "ImGuiDrawView.h"
-#import "IMGUI/imgui.h"
-#import "IMGUI/imgui_internal.h"
-#import "IMGUI/imgui_impl_apple.h"
-
 @implementation ImGuiDrawView
-// Empty clean view for build
 @end
